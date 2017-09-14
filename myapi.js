e0c3fb3ffda07e98157f453b5be1e51062432e0f
DimTrigkakis <jim.tr@hotmail.com>
2017-09-14 01:51:46 -0700

    Create myapi.js

---
+++ myapi.js
@@ -0,0 +1,13 @@
+
+function myFunction() {
+
+    document.getElementById("demo").innerHTML = httpGet("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=RGAPI-acaad9bd-dae4-4dcf-ba96-e3ab026db416");
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
