2c3879207e861de20c6b84230f872a3367a866b9
DimTrigkakis <jim.tr@hotmail.com>
2017-09-20 18:33:33 -0700

    Update myapi.js

--- myapi.js
+++ myapi.js
@@ -1,8 +1,12 @@
 
+function get(name){
+   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+      return decodeURIComponent(name[1]);
+}
+
 function myFunction() {
     
-    document.getElementById("demo").innerHTML = httpGet("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=RGAPI-acaad9bd-dae4-4dcf-ba96-e3ab026db416");
-
+    document.getElementById("demo").innerHTML = get(httpGet("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=RGAPI-acaad9bd-dae4-4dcf-ba96-e3ab026db416"));
     alert("Hello world");
 }
 
